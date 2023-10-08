92760e3f5c520b8c8d2418f6f14e45d52498daa7
Sang Jun <111563955+pedro1798@users.noreply.github.com>
2023-10-08 21:18:57 +0900

    dp initiation change

--- Platinum/11438.py
+++ Platinum/11438.py
@@ -5,7 +5,7 @@ import math
 def dfs(root: int, visited: list, adj_list: list, dp: list) -> int:
     if not visited[root]:  # visited False면
         visited[root] = True
-        for child in adj_list[root]:
+        for child in adj_list[root]:  # root의 자식 노드들
             if not visited[child]:
                 dp[child][0] = root
                 depth[child] = depth[root] + 1
@@ -30,9 +30,10 @@ def lca(n: int, m: int, dp: list, depth: list) -> int:  # longest common ancesto
     return dp[n][0]
 
 N = int(ss.readline())  # 노드 1~N
-adj_list = [[0] for i in range(N + 1)]
+adj_list = [list() for i in range(N + 1)]  # 인접 리스트
 depth = [0 for i in range(N + 1)]  # 깊이 array
 
+    
 for i in range(N - 1):  # 인접 리스트로 트리 초기화
     n, m = map(int, ss.readline().split())
     adj_list[min(n, m)].append(max(n, m))
@@ -41,21 +42,20 @@ visited = [False for _ in range(N + 1)]
 dp = [[0] * 17 for nodes in range(N + 1)]
 dfs(1, visited,adj_list, dp)
 
-M = int(ss.readline())
-nodes = []
+    
+M = int(ss.readline())  # LCA 찾을 노드쌍 개수
+
+nodes = []  # LCA 찾을 노드쌍 입력
 for i in range(M):
     nodes.append(tuple(map(int, ss.readline().split())))
 
 # dp table 채움
 # math.ceil(log2(트리의 높이)) 만큼만 dp테이블 채우면 됨.
 max_height = math.ceil(math.log2(max(depth)))
-print('!!!!!111', max_height)
 for k in range(max_height):
-    for node in range(N + 1):
+    for node in range(1, N + 1):
         dp[node][k + 1] = dp[dp[node][k]][k]
-
-for i in dp:
-    print(*i)
+        
 
 for n, m in nodes:
     print(lca(n, m, dp, depth))
