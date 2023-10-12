30ac3b39b1005fb374d5e24e33139148c712c3eb
Sang Jun <111563955+pedro1798@users.noreply.github.com>
2023-10-12 18:50:48 +0900

    Update 14003.py

--- Platinum/14003.py
+++ Platinum/14003.py
@@ -2,6 +2,7 @@ from sys import stdin as ss
 n = int(ss.readline())
 a = list(map(int, ss.readline().split()))
 
+
 def binary_search(result: list, target: int) -> int:
     l = 0; r = len(result) - 1
     while l <= r:
@@ -10,12 +11,12 @@ def binary_search(result: list, target: int) -> int:
             r = m - 1
         else:
             l = m + 1
-    return l
-    
+    return l  # target 이상의 값 중 가장 작은 값 인덱스 return
+
 
 def lis(a: list):
-    result = [a[0]]
-    index = [0] * len(a)
+    result = [a[0]]  # len(result) == lis의 길이
+    index = [0] * len(a)  # lis 구하기 위해 인덱스를 저장
     for i in range(1, len(a)):
         if a[i] > result[-1]:
             index[i] = len(result)
@@ -24,8 +25,8 @@ def lis(a: list):
             insert = binary_search(result, a[i])
             index[i] = insert
             result[insert] = a[i]
-            
-    s = list()
+
+    s = list()  # lis 중 하나, 뒤에서부터 탐색해 뒤집어 return
     count = len(result) - 1
     for j in range(len(a) - 1, -1, -1):
         if index[j] == count:
@@ -33,5 +34,6 @@ def lis(a: list):
             count -= 1
     print(len(result))
     print(*reversed(s))
-    
+
+
 lis(a)
