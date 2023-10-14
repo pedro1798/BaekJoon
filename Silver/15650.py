06ee907a5b719e502c65b514e33d63469bff2419
Sang Jun <111563955+pedro1798@users.noreply.github.com>
2023-10-14 09:06:29 +0900

    Update 15650.py

--- Silver/15650.py
+++ Silver/15650.py
@@ -3,6 +3,6 @@ from sys import stdin as ss
 
 n, r = map(int, ss.readline().split())
 arr = [i for i in range(1, n + 1)]
-nPr = itertools.combinations(arr, r)
-for i in list(nPr):
+nCr = itertools.combinations(arr, r)
+for i in list(nCr):
     print(*i)
