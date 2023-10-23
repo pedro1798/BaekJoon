3a37e61b34a3ae95eea34429b36256e2a8c30e06
Sang Jun <111563955+pedro1798@users.noreply.github.com>
2023-10-23 23:32:41 +0900

    Update 1708.py

--- Platinum/1708.py
+++ Platinum/1708.py
@@ -1,4 +1,4 @@
-# Fucking Convex Hull
+# Convex Hull
 
 from sys import stdin as ss
 import math
@@ -13,19 +13,24 @@ class Problem:
             self.points.append((x, y))
         
     def get_angle(self, pivot: tuple, point: tuple) -> list:
-        return math.atan2(point[1] - pivot[1], point[0] - pivot[0])
+        # return math.atan2(point[1] - pivot[1], point[0] - pivot[0])
+        # 기울기가 atan2보다 빠르다
+        return math.inf if point[0] == pivot[0] else (point[1] - pivot[1]) / (point[0] - pivot[0])
     
     def ccw(self, a: tuple, b: tuple, c: tuple):
+        # 외적의 z성분으로 세 노드의 방향성 도출
         ab = (b[0] - a[0], b[1] - a[1])
         ac = (c[0] - a[0], c[1] - a[1])
         return (ab[0] * ac[1] - ab[1] * ac[0]) > 0
     
     def graham_scan(self, points: list) -> list:
-    
         pivot = min(points, key=lambda x: (x[0], x[1]))
+        # 반시계방향 정렬할 기준점
         points.remove(pivot)
         
-        sorted_points = sorted(points, key=lambda x: (self.get_angle(pivot, x), x[0], x[1]))
+        sorted_points = sorted(points, key=lambda x: (self.get_angle(pivot, x), x[0], -x[1])) + [pivot]
+        # 반시계방향으로 정렬 위해 y값 같으면 x는 오름차순, x값 같으면 y는 내림차순
+        # 마지막에 원점과의 비교 위해 [pivot] append
 
         convex_hull = [pivot, sorted_points.pop(0)]
         
@@ -34,6 +39,9 @@ class Problem:
                 convex_hull.pop()
             convex_hull.append(item)
             
+        convex_hull.pop()
+        # 원점과의 비교 끝나면 pop()
+        
         return convex_hull
     
     def main(self):
