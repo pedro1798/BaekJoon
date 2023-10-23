5e466bf4887b8158af75198ac6c0442feb0c959e
Sang Jun <111563955+pedro1798@users.noreply.github.com>
2023-10-23 23:37:43 +0900

    Update 4181.py

--- Platinum/4181.py
+++ Platinum/4181.py
@@ -16,25 +16,6 @@ class Problem:
     def angle_sort(self, pivot: tuple, points: list) -> list:
         return sorted(points, key=lambda x:(self.get_angle(pivot, x), -x[1], x[0]))
     
-    def ccw(self, a: tuple, b: tuple, c: tuple):
-        ab = (b[0] - a[0], b[1] - a[1])
-        ac = (c[0] - a[0], c[1] - a[1])
-        return ab[0] * ac[1] - ab[1] * ac[0] > 0
-    
-    def graham_scan(self, points: list):
-        pivot = min(points, key=lambda x: (x[0], x[1]))
-        points.remove(pivot)
-        
-        sorted_points = self.angle_sort(pivot, points)
-        convex_hull = [pivot, sorted_points.pop(0)]
-        
-        for item in sorted_points:
-            while len(convex_hull) >= 2 and (not self.ccw(convex_hull[-2], convex_hull[-1], item)):
-                convex_hull.pop()
-            convex_hull.append(item)
-            
-        return convex_hull
-    
     def main(self):
         pivot = min(self.points, key=lambda x: (x[0], x[1]))
         self.points.remove(pivot)
